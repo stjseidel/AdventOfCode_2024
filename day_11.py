13f1701aba941cb8253799bbc07a74fc59a8a713
Stefan Seidel <stefan@seidel.engineering>
2024-12-11 21:27:37 +0100

    add day11_2

--- day_11.py
+++ day_11.py
@@ -16,58 +16,65 @@ class Today(AOC):
         lines = self.lines
         self.stone_lists = [[int(chars) for chars in line.split(' ')] for line in lines]
         # lines = [[int(lin) for lin in line.split(' ') if set(lin) != set('') ] for line in lines]
+        self.memo = {}
         return lines
     
     def part1(self):
-        lines = self.parse_lines()
-        self.process_stones(blinks=25)
-        self.result1 = self.total_stones
-        # print(self.blink_result_list)
+        _ = self.parse_lines()
+        total_stones = self.process_stones(blinks=25)
+        self.result1 = total_stones 
         self.time1 = timer()
         return self.result1
 
-    def process_stones(self, blinks=25):
-        self.blinks = blinks        
-        self.total_stones = 0
-        self.blink_result_list = []
-        for stones in self.stone_lists:
-            for stone in stones:
-                self.blink_stone(stone=stone, current_blink=0)
-                
-                
-    def blink_stone(self, stone, current_blink):
-        """Process a stone recursively until the max blinks have been reached
-        If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-        If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-        If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
+    def process_stones(self, blinks):
+         """
+         Initialize the recursive process for the given number of blinks.
+         """
+         self.blinks = blinks
+         self.memo = {}  
+         initial_stones = self.stone_lists[0]
+    
+         total_result = 0
+         for stone in initial_stones:
+             total_result += self.blink_stone(stone, blinks)
+    
+         return total_result
+  
+    def blink_stone(self, stone, remaining_blinks):
         """
-        if current_blink >= self.blinks:
-            self.total_stones += 1
-            # self.blink_result_list.append(stone)
-            return None
-        if stone == 0:
-            self.blink_stone(1, current_blink+1)
-            return None
-        stone_chars = str(stone) 
-# %%
-        stone_char_length = len(stone_chars)
-# %%
+        Process a stone recursively, summing up results for the remaining blinks.
+        """
+        if remaining_blinks == 0:
+            return 1
 
-# =============================================================================
-# 
-# =============================================================================
-        if stone_char_length % 2 == 0:
-            left, right = int(stone_chars[stone_char_length // 2:]), int(stone_chars[:stone_char_length // 2])
-            self.blink_stone(stone=left, current_blink=current_blink+1)
-            self.blink_stone(stone=right, current_blink=current_blink+1)
-            return None
-        self.blink_stone(stone=stone*2024, current_blink=current_blink+1)
-        return None
-        
+        if (stone, remaining_blinks) in self.memo:
+            return self.memo[(stone, remaining_blinks)]
+
+        if stone == 0:
+            result = self.blink_stone(1, remaining_blinks - 1)
+        else:
+            stone_chars = str(stone)
+            stone_char_length = len(stone_chars)
+
+            if stone_char_length % 2 == 0:
+                midpoint = stone_char_length // 2
+                left = int(stone_chars[:midpoint]) 
+                right = int(stone_chars[midpoint:])
+                result = (
+                    self.blink_stone(left, remaining_blinks - 1)
+                    + self.blink_stone(right, remaining_blinks - 1)
+                )
+            else:
+                result = self.blink_stone(stone * 2024, remaining_blinks - 1)
+
+        self.memo[(stone, remaining_blinks)] = result
+        return result
+
+
     def part2(self):
-        lines = self.parse_lines()
-        self.process_stones(blinks=75)
-        self.result2 = self.total_stones
+        _ = self.parse_lines()
+        total_stones = self.process_stones(blinks=75)
+        self.result2 = total_stones 
         self.time2 = timer()
         return self.result2
         
@@ -93,16 +100,14 @@ if __name__ == '__main__':
     today.stop()
 
 
-# # simple part 2
-#     today.set_lines(simple=True) 
-#     today.part2()
-#     print(f'Part 2 <SIMPLE> result is: {today.result2}')
+# simple part 2
+    today.set_lines(simple=True) 
+    today.part2()
+    print(f'Part 2 <SIMPLE> result is: {today.result2}')
 
-# =============================================================================
-# # hard part 2
-#     today.set_lines(simple=False)
-#     today.part2()
-#     print(f'Part 2 <HARD> result is: {today.result2}')
-#     today.stop()
-#     today.print_final()
-# =============================================================================
+# hard part 2
+    today.set_lines(simple=False)
+    today.part2()
+    print(f'Part 2 <HARD> result is: {today.result2}')
+    today.stop()
+    today.print_final()
